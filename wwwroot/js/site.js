a7c79df1a9b112278b66b624abd0a2a73472600f
Steven Vore <steven@vorefamily.net>
2016-09-12 19:34:47 -0400

    unsure

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -6,9 +6,12 @@
     var main = $("#main");
     main.on("mouseenter", function () {
         $( this ).css( "background-color", "#888" );
+        // not sure why this syntax does not work for me.
+        // main.style = "background-color: #888;";
     });
 
     main.on("mouseleave", function () {
         $( this ).css( "background-color", "#eee" );
     });
+
 })();
