f9633243b974979baf2cc2a55bd2068f9d2d9a4b
Steven Vore <steven@vorefamily.net>
2016-09-13 15:06:57 -0400

    do something useful with jQuery - showing/hiding the side panel.

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,20 +1,12 @@
 // site.js
 (function () {
-    var ele = $("#username");
-    ele.text("Grumpy Grandpa");
-
-    var main = $("#main");
-    main.on("mouseenter", function () {
-        main.css("background-color", "#888" );
-    });
-
-    main.on("mouseleave", function () {
-        main.css("background-color", "#eee" );
-    });
-
-    var menuItems = $("ul.menu li a");
-    menuItems.on("click", function() {
-        var me = $(this);
-        alert(me.text());
+    var $sidebarAndWrapper = $("#sidebar,#wrapper"); //jQuery wrapped set
+    $("#sidebarToggle").on("click", function() {
+        $sidebarAndWrapper.toggleClass("hide-sidebar");
+        if ($sidebarAndWrapper.hasClass("hide-sidebar")) {
+            $(this).text("Show Sidebar");
+        } else {
+            $(this).text("Hide Sidebar");
+        }
     });
 })();
