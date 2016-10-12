3c415bc84fa2fb60579bfff6f2919c25b7a2d1f9
Steven Vore <steven@vorefamily.net>
2016-10-12 08:48:12 -0400

    only show errors on developers' systems

--- Startup.cs
+++ Startup.cs
@@ -18,7 +18,10 @@ namespace hwcode
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
-            app.UseDeveloperExceptionPage();
+            if (env.IsEnvironment("Development")) {
+                //zsh: export ASPNETCORE_ENVIRONMENT=Development
+                app.UseDeveloperExceptionPage();
+            }
 
             app.UseStaticFiles();
 
