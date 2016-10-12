64e9524ebd5cb7943594c37ef18e82f325e78020
Steven Vore <steven@vorefamily.net>
2016-10-12 08:34:54 -0400

    a couple more pages, one with an intentional exception for demonstration

--- Controllers/Web/AppController.cs
+++ Controllers/Web/AppController.cs
@@ -1,3 +1,4 @@
+using System;
 using Microsoft.AspNetCore.Mvc;
 
 namespace TheWorld.Controllers.Web
@@ -5,6 +6,15 @@ namespace TheWorld.Controllers.Web
     public class AppController : Controller
     {
         public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            throw new InvalidOperationException("bad things happen to good developers.");
+            //return View();
+        }
+        public IActionResult About()
         {
             return View();
         }
